957ed670269b10d06ccfb4433804d3c2c4f5d3f7
ppibburr <tulnor33@gmail.com>
2018-03-15 05:41:47 -0400

    ...

--- talk.rb
+++ talk.rb
@@ -2,20 +2,25 @@
 require 'pocketsphinx-ruby'
 require 'json'
 
-def input
-  ``
-end
-
 def text
-  raw = `sox -d -b 16 -c 1 -r 16k  -t wav - trim 0 5 | curl -XPOST 'https://api.wit.ai/speech?v=20160526' -i -L -H "Authorization: Beare VM7GFFHMNBBTZZTDS5NKD43L5FW45VXE" -H "Content-Type: audio/wav" -H "Transfer-Encoding: chunked"    --data-binary @-`
-  puts raw
+  raw = `sox -d -b 16 -c 1 -r 16k  -t wav - trim 0 3.5 | curl -XPOST 'https://api.wit.ai/speech?v=20160526' -L -H "Authorization: Bearer VM7GFFHMNBBTZZTDS5NKD43L5FW45VXE" -H "Content-Type: audio/wav" -H "Transfer-Encoding: chunked" --data-binary @- -o -`
+  
   JSON.parse(raw)["_text"]
-rescue
+rescue => e
+  p e
 end
 
 c=configuration = Pocketsphinx::Configuration::KeywordSpotting.new('Alexa');
-Pocketsphinx::LiveSpeechRecognizer.new(c).recognize do |speech|
-  puts "Detected Wake: #{speech}"
-  input 
-  p text 
+r=Pocketsphinx::LiveSpeechRecognizer.new(c)
+
+r.recognize do |speech|
+begin
+  #r.pause do
+    puts "Detected Wake: #{speech}"
+
+    puts "STT: #{text}"
+  #end
+ rescue => e
+   p e
+ end
 end
